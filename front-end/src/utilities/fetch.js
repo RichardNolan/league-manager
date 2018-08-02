94feed6ac9f16765fdcc91fb316d3bdeb210fe1a
Richard Nolan <therichardnolan@gmail.com>
2018-08-02 22:20:59 +0100

    localised

--- front-end/src/utilities/fetch.js
+++ front-end/src/utilities/fetch.js
@@ -15,8 +15,8 @@ const getHeaders = ()=>(
 )
 
 
-// export const DB_HOST = 'http://localhost:9000'
-export const DB_HOST = 'https://ncileaguemanager.herokuapp.com'
+export const DB_HOST = 'http://localhost:9000'
+// export const DB_HOST = 'https://ncileaguemanager.herokuapp.com'
 
 export const getStandard = ()=>(
     Object.assign({},
